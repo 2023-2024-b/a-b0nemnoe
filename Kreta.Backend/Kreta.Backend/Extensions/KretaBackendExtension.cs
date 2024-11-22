a45b8ebc7d1d68b71619e56767318b4b72e6742f
b0nemnoe <b0nemnoe@vasvari.org>
2024-11-22 10:35:04 +0100

    KretabackEndExtension

---
+++ Kreta.Backend/Kreta.Backend/Extensions/KretaBackendExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace Kreta.Backend.Extensions
+{
+    public static class KretaBackendExtension
+    {
+
+        public static void AddBackend(this IServiceCollection services)
+        {
+
+        }
+
+        private static void ConfigureCors(this IServiceCollection services) 
+        {
+            services.AddCors(option => option.AddPolicy(name: "KretaCors", policy => {
+                policy.WithOrigins("https://0.0.0.0:7020/")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            }));
+
+
+        }
+
+
+
+    }
+}
